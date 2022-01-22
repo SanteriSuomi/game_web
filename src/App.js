a14c88219b76d6bcf7756d5bce4cee285b7d33bd
Santeri Suomi <sant.suomi@gmail.com>
2022-01-23 01:55:59 +0200

    Start building the frontend. Simple navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center, Link, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import "@fontsource/bungee";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Box className="NavBox">
+				<Wrap
+					className="NavBoxWrap"
+					spacing="3.5%"
+					justify="center"
+					align="center"
+				>
+					<WrapItem>
+						<Center className="NavBoxTextTitleBox">
+							<Text className="NavBoxTextTitle">
+								Slime Keeper
+							</Text>
+						</Center>
+					</WrapItem>
+					<WrapItem>
+						<Link href="/features">
+							<Text className="NavBoxText">About</Text>
+						</Link>
+					</WrapItem>
+					<WrapItem>
+						<Link href="/tokenomics">
+							<Text className="NavBoxText">Tokenomics</Text>
+						</Link>
+					</WrapItem>
+					<WrapItem>
+						<Link href="/team">
+							<Text className="NavBoxText">Team</Text>
+						</Link>
+					</WrapItem>
+					<WrapItem>
+						<Link href="/whitepaper">
+							<Text className="NavBoxText">Whitepaper</Text>
+						</Link>
+					</WrapItem>
+				</Wrap>
+			</Box>
+		</div>
+	);
 }
 
 export default App;
