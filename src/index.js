cf2cd0ec3532be86b4e4f7f78009f8035a1bf985
Santeri Suomi <sant.suomi@gmail.com>
2022-01-26 13:34:19 +0200

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Main from "./Main";
 import Mint from "./Mint";
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<ChakraProvider>
 			<Routes>
 				<Route exact path="/" element={<Main></Main>}></Route>
 				<Route path="mint" element={<Mint></Mint>}></Route>
 			</Routes>
 		</ChakraProvider>
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById("root")
 );
